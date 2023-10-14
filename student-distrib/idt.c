6ef3ade5b9c8c2b2f3261dc8dcaac05a2fdef324
Ayodiran <soa3@illinois.edu>
2023-10-13 20:17:12 -0500

    added function headers and skeleton implementations

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -18,7 +18,7 @@ void idt_init() {
 void build_idt() {
     int i;
 
-    for (i = 0; i < IDT_LENGTH; i++) {
+    for (i = 0; i < IDT_LENGTH; i++) { //exceptions
         // idt[i].offset_15_00 = ?
         idt[i].seg_selector = KERNEL_CS;
         idt[i].reserved4 = 0;
@@ -32,16 +32,26 @@ void build_idt() {
         // idt[i].offset_31_16 = ?
 
         // for offset, we need to call SET_IDT_ENTRY
-        SET_IDT_ENTRY(idt[i], errors[i]);
+        SET_IDT_ENTRY(idt[i], exception_handler[i]);
     }
+        //128 = x80 which is int for system calls 
+        idt[128].seg_selector = KERNEL_CS;
+        idt[128].reserved4 = 0;
+        idt[128].reserved3 = 0;
+        idt[128].reserved2 = 1;
+        idt[128].reserved1 = 1;
+        idt[128].size = 1; // size of gate: 1 = 32bits, 0 = 16bits
+        idt[128].reserved0 = 0;
+        idt[128].dpl = 0; // kernel level, descriptor privilege level
+        idt[128].present = 1; // segment present flag
+
+        SET_IDT_ENTRY(idt[128], &system_call_handler);
+
+
+
 }
 
-void exception_handler(int exception_num) {
-    // do stuff
-}
 
-static inline void* errors = {divide_error, debug, nmi, breakpoint, overflow, bound_range_exceeded, invalid_opcode, device_not_available, double_fault, coprocessor_segment_overrun, 
-invalid_tss, segment_not_present, stack_fault, general_protection, page_fault, NULL, x87_fp_error, alignment_check, machin_check, simd_fp_error};
 // idt[0], divide_error
 // idt[1], debug
 // idt[2], nmi
@@ -61,3 +71,72 @@ invalid_tss, segment_not_present, stack_fault, general_protection, page_fault, N
 // idt[17], alignment_check
 // idt[18], machine_check
 // idt[19], simd_fp_error
+
+
+
+//idt[128], system call handler
+
+
+int32_t system_call_handler(uint8_t arg, int32_t fd, int32_t nbytes, void* address){
+    switch(/*something*/){
+        case /*something*/:
+            return halt(arg);
+        case /*something*/:
+            return execute((const)arg);
+        case /*something*/:
+            return read (fd, (void*)arg, nbytes);
+        case /*something*/:
+            return write (fd, (const void*)arg, nbytes);
+        case /*something*/:
+            return close (fd);
+        case /*something*/:
+            return open ((const uint8_t*) arg);
+        case /*something*/:
+            return getargs ((const uint8_t*) arg, nbytes);
+        case /*something*/:
+            return vidmap ((uint8_t**) arg);
+        case /*something*/:
+            return set_handler (nbytes /*probably bad practice but the type fits*/, address);
+        case /*something*/:
+            return sigreturn ();
+        default:
+            return -1; //failed call is -1
+        
+    }
+
+};
+
+//interrupts
+static void divide_error(){};
+static void debug(){};
+static void nmi(){};
+static void breakpoint(){}; 
+static void overflow(){}; 
+static void bound_range_exceeded(){}; 
+static void invalid_opcode(){}; 
+static void device_not_available(){}; 
+static void double_fault(){}; 
+static void coprocessor_segment_overrun(){}; 
+static void invalid_tss(){}; 
+static void segment_not_present(){}; 
+static void stack_fault(){}; 
+static void general_protection(){};
+static void page_fault(){};
+static void x87_fp_error(){};
+static void alignment_check(){};
+static void machin_check(){};
+static void simd_fp_error(){};
+
+
+
+/*system calls*/
+static inline int32_t halt (uint8_t status){};
+static inline int32_t execute (const uint8_t* command){};
+static inline int32_t read (int32_t fd, void* buf, int32_t nbytes){};
+static inline int32_t write (int32_t fd, const void* buf, int32_t nbytes){};
+static inline int32_t close (int32_t fd){};
+static inline int32_t open (const uint8_t* filename){};
+static inline int32_t getargs (uint8_t* buf, int32_t nbytes){};
+static inline int32_t vidmap (uint8_t** screen_start){};
+static inline int32_t set_handler (int32_t signum, void* handler_address){};
+static inline int32_t sigreturn (void){};
--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -9,5 +9,51 @@ void idt_init();
 /* Build idt */
 void build_idt();
 
+
+/*exception handler function pointer*/
+//https://stackoverflow.com/questions/5309859/how-to-define-an-array-of-functions-in-c
+typedef void (*exception_handler_t)();  //void just means the type is not defined, () means args undefined
+exception_handler_t exception_handler[256] = {&divide_error, &debug, &nmi, &breakpoint, &overflow, &bound_range_exceeded, &invalid_opcode, &device_not_available, &double_fault, &coprocessor_segment_overrun, 
+&invalid_tss, &segment_not_present, &stack_fault, &general_protection, &page_fault, NULL, &x87_fp_error, &alignment_check, &machin_check, &simd_fp_error};
+
+
+
+static void divide_error();
+static void debug();
+static void nmi();
+static void breakpoint(); 
+static void overflow(); 
+static void bound_range_exceeded(); 
+static void invalid_opcode(); 
+static void device_not_available(); 
+static void double_fault(); 
+static void coprocessor_segment_overrun(); 
+static void invalid_tss(); 
+static void segment_not_present(); 
+static void stack_fault(); 
+static void general_protection();
+static void page_fault();
+static void x87_fp_error();
+static void alignment_check();
+static void machin_check();
+static void simd_fp_error();
+
+
+
+
+
+extern int32_t system_call_handler(uint8_t arg, int32_t fd, int32_t nbytes, void* address);
+/*system calls*/
+static inline int32_t halt (uint8_t status);
+static inline int32_t execute (const uint8_t* command);
+static inline int32_t read (int32_t fd, void* buf, int32_t nbytes);
+static inline int32_t write (int32_t fd, const void* buf, int32_t nbytes);
+static inline int32_t close (int32_t fd);
+static inline int32_t open (const uint8_t* filename);
+static inline int32_t getargs (uint8_t* buf, int32_t nbytes);
+static inline int32_t vidmap (uint8_t** screen_start);
+static inline int32_t set_handler (int32_t signum, void* handler_address);
+static inline int32_t sigreturn (void);
+
 #endif /* IDT_H */
 
