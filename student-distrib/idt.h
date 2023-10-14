d67cb194436d1a6b2dbddd6fa799a7be55c0c4d5
Nick Xing <nxing2@illinois.edu>
2023-10-14 10:53:00 -0500

    some stuff

--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -18,42 +18,43 @@ exception_handler_t exception_handler[256] = {&divide_error, &debug, &nmi, &brea
 
 
 
-static void divide_error();
-static void debug();
-static void nmi();
-static void breakpoint(); 
-static void overflow(); 
-static void bound_range_exceeded(); 
-static void invalid_opcode(); 
-static void device_not_available(); 
-static void double_fault(); 
-static void coprocessor_segment_overrun(); 
-static void invalid_tss(); 
-static void segment_not_present(); 
-static void stack_fault(); 
-static void general_protection();
-static void page_fault();
-static void x87_fp_error();
-static void alignment_check();
-static void machin_check();
-static void simd_fp_error();
+void divide_error();
+void debug();
+void nmi();
+void breakpoint(); 
+void overflow(); 
+void bound_range_exceeded(); 
+void invalid_opcode(); 
+void device_not_available(); 
+void double_fault(); 
+void coprocessor_segment_overrun(); 
+void invalid_tss(); 
+void segment_not_present(); 
+void stack_fault(); 
+void general_protection();
+void page_fault();
+void x87_fp_error();
+void alignment_check();
+void machine_check();
+void simd_fp_error();
 
 
 
 
 
-extern int32_t system_call_handler(uint8_t arg, int32_t fd, int32_t nbytes, void* address);
-/*system calls*/
-static inline int32_t halt (uint8_t status);
-static inline int32_t execute (const uint8_t* command);
-static inline int32_t read (int32_t fd, void* buf, int32_t nbytes);
-static inline int32_t write (int32_t fd, const void* buf, int32_t nbytes);
-static inline int32_t close (int32_t fd);
-static inline int32_t open (const uint8_t* filename);
-static inline int32_t getargs (uint8_t* buf, int32_t nbytes);
-static inline int32_t vidmap (uint8_t** screen_start);
-static inline int32_t set_handler (int32_t signum, void* handler_address);
-static inline int32_t sigreturn (void);
+
+// extern int32_t system_call_handler(uint8_t arg, int32_t fd, int32_t nbytes, void* address);
+// /*system calls*/
+// static inline int32_t halt (uint8_t status);
+// static inline int32_t execute (const uint8_t* command);
+// static inline int32_t read (int32_t fd, void* buf, int32_t nbytes);
+// static inline int32_t write (int32_t fd, const void* buf, int32_t nbytes);
+// static inline int32_t close (int32_t fd);
+// static inline int32_t open (const uint8_t* filename);
+// static inline int32_t getargs (uint8_t* buf, int32_t nbytes);
+// static inline int32_t vidmap (uint8_t** screen_start);
+// static inline int32_t set_handler (int32_t signum, void* handler_address);
+// static inline int32_t sigreturn (void);
 
 #endif /* IDT_H */
 
