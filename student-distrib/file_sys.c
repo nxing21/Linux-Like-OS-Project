fffeafc6fd82e59fadc5190a2bb17786b19714d7
Nick Xing <nxing2@illinois.edu>
2023-10-23 00:36:07 -0500

    cleaning up

--- student-distrib/file_sys.c
+++ student-distrib/file_sys.c
@@ -79,8 +79,7 @@ int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry) {
     dentry_t * dentries_array = boot_block->direntries;
     uint32_t num_dentry = boot_block->dir_count;
 
-    if( index < num_dentry)
-    {
+    if(index < num_dentry) {
         *dentry = dentries_array[index];
         return 0;
     } 
