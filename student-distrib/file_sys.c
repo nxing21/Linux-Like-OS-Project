a84676bfe31c7d360bac83826f08ea366a6a7fb8
Xing <nxing2@illinois.edu>
2023-10-28 17:10:52 -0500

    did some work on execute

--- student-distrib/file_sys.c
+++ student-distrib/file_sys.c
@@ -60,7 +60,7 @@ int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry) {
 
     if(found_flag == 1) {
         *dentry = found_dentry;
-        dentry->filename[32] = '\0';
+        dentry->filename[FILENAME_LEN] = '\0';
         return 0;
     }
     printf("didn't find");
--- student-distrib/syscalls.c
+++ student-distrib/syscalls.c
@@ -1,9 +1,37 @@
 #include "file_sys.h"
 #include "syscalls.h"
-
+#include "lib.h"
 
 int32_t system_execute(const uint8_t* command) {
+    uint8_t filename[FILENAME_LEN + 1];
+    if (command == NULL) {
+        return -1;
+    }
+    filename[FILENAME_LEN] = '\0';
+    int i; // loop counter
 
+    // get the name of the executable
+    while (command[i] != '\0') {
+        if (command[i] == ' ') {
+            break;
+        }
+        else {
+            filename[i] = command[i];
+        }
+        i++;
+    }
+    filename[i] = '\0';
+
+    dentry_t* dentry;
+    // check the validity of the filename
+    if (read_dentry_by_name(filename, dentry) == -1) {
+        return -1;
+    }
+
+    // Set up paging
+
+    // Flush TLB
+    asm volatile ();
 }
 
 int32_t system_halt(uint8_t status) {
@@ -11,13 +39,13 @@ int32_t system_halt(uint8_t status) {
 }
 
 
-int32_t read (int32_t fd, void* buf, int32_t nbytes){
+int32_t read (int32_t fd, void* buf, int32_t nbytes) {
 
 }
-int32_t write (int32_t fd, const void* buf, int32_t nbytes){
+int32_t write (int32_t fd, const void* buf, int32_t nbytes) {
 
 }
-int32_t open (const uint8_t* filename){
+int32_t open (const uint8_t* filename) {
     dentry_t temp_dentry;
     uint32_t file_type;
 
@@ -32,6 +60,6 @@ int32_t open (const uint8_t* filename){
         return -1;
     }
 }
-int32_t close (int32_t fd){
+int32_t close (int32_t fd) {
 
 }
