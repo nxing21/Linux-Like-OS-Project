0be9c633ec2497c5067e12937107a631967a2572
Sam Ayodiran <soa3@illinois.edu>
2023-11-30 00:56:54 -0600

    gave each terminal an rtc counter

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -4,13 +4,16 @@
 #include "lib.h"
 #include "nmi.h"
 #include "syscalls.h"
+#include "terminal.h"
 
 #define RTC_IRQ         8
 #define BYTE_4          4
 #define RATE_OFFSET     3
 
 int RTC_frequency, RTC_max_counter;
-volatile int RTC_block, RTC_counter;
+// volatile int RTC_block, RTC_counter;
+volatile int RTC_block;
+volatile int RTC_counters[MAX_TERMINALS]; // each terminal gets a separate RTC counter
 
 /* 
  * init_RTC
@@ -60,7 +63,7 @@ void set_RTC_frequency(int freq) {
     if (freq >= rtc_min_frequency && freq <= rtc_max_usable_frequency) {
         RTC_frequency = freq;
         RTC_max_counter = rtc_max_usable_frequency / RTC_frequency;
-        RTC_counter = RTC_max_counter;
+        RTC_counters[curr_terminal] = RTC_max_counter;
     }
 }
 
@@ -83,11 +86,11 @@ void RTC_handler() {
     garbage = inb(RTC_REGISTER_DATA_PORT);
 
     // Sets RTC_counter for RTC_read
-    if (RTC_counter == 0) {
+    if (RTC_counters[curr_terminal] == 0) {
         RTC_block = 0;
-        RTC_counter = RTC_max_counter;
+        RTC_counters[curr_terminal] = RTC_max_counter;
     } else {
-        RTC_counter--;
+        RTC_counters[curr_terminal]--;
     }
 
     send_eoi(RTC_IRQ);
@@ -160,6 +163,7 @@ int32_t RTC_write(int32_t fd, const void* buffer, int32_t nbytes) {
     
     // obtains frequency from buffer
     freq = *((unsigned int*)(buffer));
+
 
     // loops through all possible rates
     for (i = rtc_lowest_rate; i <= rtc_highest_rate; i++) {
